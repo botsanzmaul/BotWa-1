049385d798e637b86c8f24746274d3a87aed175b
nizambot <92614111+nizambot@users.noreply.github.com>
2021-10-20 03:49:20 +0700

    Add files via upload

--- dha.js
+++ dha.js
@@ -44,7 +44,7 @@ const timeWib = moment.tz('Asia/Jakarta').format('DD/MM')
 const Exif = require('./lib/exif');
 const exif = new Exif();
 
-const { downloadMenu, infoMenu, gameMenu, groupMenu, funMenu, wibuMenu, ownerMenu, stickerMenu, otherMenu, rulesBot, islamMenu, sertiMenu, ceritaMenu, iklanBot, makerMenu,dewasaMenu, toolsMenu} = require('./message/help.js')
+const { downloadMenu, infoMenu, gameMenu, groupMenu, funMenu, wibuMenu, ownerMenu, stickerMenu, otherMenu, rulesBot, islamMenu, sertiMenu, ceritaMenu, makerMenu, dewasaMenu, toolsMenu} = require('./message/help.js')
 const { getBuffer, getGroupAdmins, getRandom, runtime, sleep } = require('./lib/myfunc')
 const { fetchJson, getBase64, kyun, createExif } = require('./lib/fetch')
 const { color, bgcolor } = require('./lib/color')
@@ -4206,7 +4206,7 @@ case 'hadist':
       get_args = args.join(" ").split("|")
       kitab = get_args[0]
       nomor = get_args[1]
-      var hadist = await fetchJson('https://api-psycho.herokuapp.com/api/hadits?kitab=${kitab}&nomor=${nomor}&apikey=${setting.psyco}')
+      var hadist = await fetchJson(`https://api-psycho.herokuapp.com/api/hadits?kitab=${kitab}&nomor=${nomor}&apikey=${setting.psyco}`)
          ini_result = hadist.result
          ini_txt = `Name : ${ini_result.name}\n`
          ini_txt += `Id : ${ini_result.id}\n`
@@ -4220,7 +4220,7 @@ case 'hadist':
 case 'kisahnabi':
                 if (args.length == 0) return reply(`Example: ${prefix + command} Muhammad`)
                     query = args.join(" ")
-                    get_result = await fetchJson(`https://api.lolhuman.xyz/api/kisahnabi/${query}?apikey=9dff22db324aa3e93a246025`)
+                    get_result = await fetchJson(`https://api-psycho.herokuapp.com/api/kisahnabi?nabi=${query}&apikey=${setting.psyco}`)
                     get_result = get_result.result
                     ini_txt = `Name : ${get_result.name}\n`
                     ini_txt += `Lahir : ${get_result.thn_kelahiran}\n`
