b2234009fee783609a5fde7f67cd38fea02c2f6b
iyyan18 <92730102+iyyan18@users.noreply.github.com>
2021-11-05 10:48:37 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -10,7 +10,7 @@ module.exports.mess = {
 			only: {
 				group: 'KHUSUS GRUP BRO',
 				admin: 'KHUSUS ADMIN GRUP BRO',
-				premium: 'LU BUKAN MEMBER PREMIUM,BELI DULU SANA',
+				premium: 'Khusus Premium',
 				owner: 'KHUSUS OWNER BRO',
 				Badmin: 'BOT HARUS JADI ADMIN BRO',
 			}
